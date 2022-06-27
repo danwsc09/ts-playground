3d6f4f96b6de0719f70368c587a6bef25acf9225
Wonsang <sang.jeong@freedgrouptech.com>
2022-06-27 21:53:28 +0900

    solution to challenge 2

--- ts-chal/c2.ts
+++ ts-chal/c2.ts
@@ -1,4 +1,12 @@
-export const getDeepValue = (obj: any, firstKey: string, secondKey: string) => {
+export const getDeepValue = <
+  TObj,
+  TFirstKey extends keyof TObj,
+  TSecondKey extends keyof TObj[TFirstKey]
+>(
+  obj: TObj,
+  firstKey: TFirstKey,
+  secondKey: TSecondKey
+) => {
   return obj[firstKey][secondKey];
 };
 
