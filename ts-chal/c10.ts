24aa3e69c9e20721ccf4237f41d5826d117340fc
Wonsang <sang.jeong@freedgrouptech.com>
2022-06-27 23:22:03 +0900

    "add challenge 10 question"

---
+++ ts-chal/c10.ts
@@ -0,0 +1,26 @@
+import { String, Union } from "ts-toolbelt";
+
+const query = `/home?a=foo&b=wow`;
+
+// how to type the query string so that we get
+// { a: string, b: string }
+
+type Query = typeof query;
+
+type SecondQueryPart = String.Split<Query, "?">[1];
+
+type QueryElements = String.Split<SecondQueryPart, "&">;
+
+type QueryParams = {
+  [QueryElement in QueryElements[number]]: {
+    [Key in String.Split<QueryElement, "=">[0]]: String.Split<
+      QueryElement,
+      "="
+    >[1];
+  };
+}[QueryElements[number]];
+
+const obj: Union.Merge<QueryParams> = {
+  a: "foo",
+  b: "wow",
+};
