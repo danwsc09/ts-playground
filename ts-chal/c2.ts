0cef5dc08d6641d00954be3933a23a57d968edd8
Wonsang <sang.jeong@freedgrouptech.com>
2022-06-27 21:54:20 +0900

    add zod library for typing API responses
    types the response at compile time AND run time and throws a runtime error if types aren't an exact match

--- ts-chal/c2.ts
+++ ts-chal/c2.ts
@@ -28,3 +28,34 @@ const value = getDeepValue(obj, "foo", "a");
  * Ex. const value = getDeepValue(obj, "foo", "a");  // value should have type boolean
  * const value = getDeepValue(obj, "foo", "b"); // value should have type number
  */
+
+const returnWhatIPassIn = <T>(val: T): T => {
+  return val;
+};
+
+let res = returnWhatIPassIn(123);
+let res1 = returnWhatIPassIn("hello, world");
+
+// yarn add zod - for typing API responses where you don't know exactly what the type will be
+// this will throw a runtime error if the types don't match
+
+import { z } from "zod";
+
+const Data = z.object({
+  id: z.string(),
+  name: z.string(),
+});
+
+type DataType = z.infer<typeof Data>;
+
+// This won't throw any errors even if the API doesn't actually contain the field 'id' or 'name'
+interface DataFromApi {
+  id: string;
+  name: string;
+}
+
+fetch("/something")
+  .then((res) => res.json())
+  .then((result) => {
+    const data = Data.parse(result); // this is now typed, and will throw errors if API doesn't have these exact fields
+  });
