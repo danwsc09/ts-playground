3608a675a4437b4b07a80f51bb43cea9a6190dbd
Wonsang <sang.jeong@freedgrouptech.com>
2022-06-27 23:19:05 +0900

    add solution to challenge 9

--- ts-chal/c9.ts
+++ ts-chal/c9.ts
@@ -1,10 +1,10 @@
 // how to check if params (a, b) are arrays and prevent arrays
 // using types, instead of isArray syntax?
 
-export const deepEqualCompare = <Arg>(a: Arg, b: Arg): boolean => {
-  if (Array.isArray(a) || Array.isArray(b)) {
-    throw new Error("You cannot compare two arrays using deepEqualCompare");
-  }
+export const deepEqualCompare = <Arg>(
+  a: Arg extends any[] ? `Don't pass an array` : Arg,
+  b: Arg
+): boolean => {
   return a === b;
 };
 
