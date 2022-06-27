0bb8a9bdfac388871c3c6d1996cb811e54fac10b
Wonsang <sang.jeong@freedgrouptech.com>
2022-06-27 22:44:48 +0900

    add question prompt for challenge 3

--- ts-chal/c3.ts
+++ ts-chal/c3.ts
@@ -7,8 +7,12 @@ type Human = {
   lastName: string;
 };
 
-type GetRequiredInformation<TType> = any;
+type GetRequiredInformation<TType> = TType;
 
 export type RequiredInformationForAnimal = GetRequiredInformation<Animal>;
 
 export type RequiredInformationForHuman = GetRequiredInformation<Human>;
+
+// how to type GetRequiredInformation so that
+// for humans, we need to get their SSN field, and
+// for animals, we need to get their age field?
